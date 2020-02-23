b0d003be9405907717208a7f12cb1aa7eab4d50b
Oli <dev.olivercarlos@gmail.com>
2020-02-23 22:19:01 +0800

    Add vuejs-datepicker. Add mutation and actions for creating a new event.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,45 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import EventService from "@/services/EventService.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    user: {
+      id: "abc123",
+      name: "Oliver Carlos"
+    },
+    categories: [
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "community"
+    ],
+    events: []
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event);
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit("ADD_EVENT", event);
+      });
+    }
+  },
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length;
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id);
+    }
+  }
 });
