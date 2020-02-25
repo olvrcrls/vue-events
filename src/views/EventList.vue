fc0fd5cd760a24227a51132069aeba40e3b7fcf1
Oli <dev.olivercarlos@gmail.com>
2020-02-25 12:01:05 +0800

    Implement pagination and add router full path on key to refresh router view.

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,21 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+      >
+        Previous Page
+      </router-link>
+      |
+    </template>
+    <router-link
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+    >
+      Next Page
+    </router-link>
   </div>
 </template>
 
@@ -11,10 +26,13 @@ import { mapState } from "vuex";
 export default {
   components: { EventCard },
   created() {
-    this.$store.dispatch("fetchEvents");
+    this.$store.dispatch("fetchEvents", { perPage: 3, page: this.page });
   },
   computed: {
-    ...mapState(["events"])
+    ...mapState(["events"]),
+    page() {
+      return parseInt(this.$route.query.page) || 1;
+    }
   }
 };
 </script>
