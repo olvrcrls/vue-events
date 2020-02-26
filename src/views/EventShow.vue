570d6daa8f41dbeebf6541ce955556371e57b7ed
Oli <dev.olivercarlos@gmail.com>
2020-02-26 14:37:07 +0800

    Add NProgress module. Implement NProgress with VueRouter hooks.

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -36,19 +36,28 @@
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState } from "vuex";
+import NProgress from "nprogress";
+import store from "@/store";
 export default {
   props: ["id"],
-  created() {
-    this.fetchEvent(this.id);
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    NProgress.start();
+    store
+      .dispatch("event/fetchEvent", routeTo.params.id)
+      .then(() => {
+        NProgress.done();
+        next();
+      })
+      .catch(() => {
+        NProgress.done();
+        next(false);
+      });
   },
   computed: {
     ...mapState({
       event: state => state.event.event
     })
-  },
-  methods: {
-    ...mapActions("event", ["fetchEvent"])
   }
 };
 </script>
